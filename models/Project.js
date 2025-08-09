c29046f44ffac58fe832fbd4c5a08bdb1946afa8
kaha81 <abdlhaseeb17@gmail.com>
2025-08-09 07:43:45 +0500

    minor fix

--- models/Project.js
+++ models/Project.js
@@ -21,7 +21,8 @@ const projectSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, default: '' },
     tasks: [taskSchema],
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
 });
+
 
 module.exports = mongoose.model('Project', projectSchema);
